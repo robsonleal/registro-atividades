7a95e47604a9a094c7dd0767c182d0490a4ff85e
Robson Leal <robson_leal@outlook.com.br>
2023-02-15 19:49:53 -0300

    Adcionando arquivos do projeto backend

---
+++ backend/src/main/java/com/br/prodigi/ProdigiApplication.java
@@ -0,0 +1,13 @@
+package com.br.prodigi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProdigiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProdigiApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/br/prodigi/ProdigiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.prodigi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProdigiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
