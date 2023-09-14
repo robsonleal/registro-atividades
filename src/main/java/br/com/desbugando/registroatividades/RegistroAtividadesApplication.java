60013e6a2cc38dad2bb1c80cb42a8a454fa0a51d
Robson Leal <robson_leal@outlook.com.br>
2023-09-14 16:35:03 -0300

    First commit

---
+++ src/main/java/br/com/desbugando/registroatividades/RegistroAtividadesApplication.java
@@ -0,0 +1,13 @@
+package br.com.desbugando.registroatividades;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegistroAtividadesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegistroAtividadesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/desbugando/registroatividades/RegistroAtividadesApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.desbugando.registroatividades;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistroAtividadesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
